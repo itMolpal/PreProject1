74f8e4dbcc0ec11365c88a088b48a5a01ecd7581
Anatolii Kiselev <itmolpal@gmail.com>
2024-09-18 09:56:41 +0300

    Hibernate solution

---
+++ Hibernate/src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,28 @@
+package jm.task.core.jdbc;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.*;
+
+public class Main {
+    private final static UserService userService = new UserServiceImpl();
+    public static void main(String[] args) {
+        // реализуйте алгоритм здесь
+        userService.createUsersTable();
+        userService.saveUser("Vitalii", "Sorokin", (byte) 25);
+        userService.saveUser("Misha", "Voronin", (byte) 54);
+        userService.saveUser("Oleg", "Govorov", (byte) 32);
+        userService.saveUser("Anna", "Denisova", (byte) 19);
+//        userService.removeUserById(2);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
+
+
+
+    }
+}
---
+++ Hibernate/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -0,0 +1,42 @@
+package jm.task.core.jdbc.service;
+
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    UserDao userDao = new UserDaoJDBCImpl();
+
+    public void createUsersTable() {
+        userDao.createUsersTable();
+
+    }
+
+    public void dropUsersTable() {
+userDao.dropUsersTable();
+    }
+
+    public void saveUser(String name, String last_name, byte age) {
+        userDao.saveUser(name, last_name, age);
+
+    }
+
+    public void removeUserById(long id) {
+        userDao.removeUserById(id);
+    }
+
+    public List<User> getAllUsers() {
+        List<User> users =  userDao.getAllUsers();
+        for (User user : users) {
+            System.out.println(user);
+        }
+        return users;
+    }
+
+    public void cleanUsersTable() {
+        userDao.cleanUsersTable();
+
+    }
+}
